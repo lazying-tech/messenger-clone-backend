9386672110584ebbc7366b18ecb1325995871d55
lazying-tech <boylmhtsingle@gmail.com>
2023-06-02 23:05:19 +0700

    Completed 1

--- src/pusher/pusher.service.ts
+++ src/pusher/pusher.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import * as Pusher from 'pusher';
+
 @Injectable()
 export class PusherService {
   pusherServer: Pusher;
   constructor() {
-    this.pusherServer = new Pusher({
+    this.pusherServer = new (Pusher as any)({
       appId: process.env.PUSHER_APP_ID!,
       key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
       secret: process.env.PUSHER_SECRET!,
